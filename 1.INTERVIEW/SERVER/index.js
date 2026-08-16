77ea9c4c28ae031d4330f7b1583d3ac62a2c9357
harshmzn111-cell <harshmzn111@gmail.com>
2026-08-16 23:09:23 +0530

    Update index.js

--- 1.INTERVIEW/SERVER/index.js
+++ 1.INTERVIEW/SERVER/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT ||  6000;
 
 app.use(cors({
-  origin: "https://ai-interview-project-client-2.onrender.com",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
