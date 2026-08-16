e0c28bea44d52fc9c28c05bedeffb20158421204
harshmzn111-cell <harshmzn111@gmail.com>
2026-08-17 00:26:25 +0530

    Update Auth.jsx

--- 1.INTERVIEW/CLIENT/src/pages/Auth.jsx
+++ 1.INTERVIEW/CLIENT/src/pages/Auth.jsx
@@ -8,7 +8,7 @@ import { motion } from "motion/react"
 import { auth, provider } from "../utils/firebase"
 import { signInWithPopup } from "firebase/auth"
 
-const serverUrl = "https://https://ai-interview-project-server.onrender.com";
+const serverUrl = "https://ai-interview-project-server.onrender.com";
 function Auth({isModel=false}) {
 const dispatch = useDispatch();
   
